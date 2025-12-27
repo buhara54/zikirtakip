80e46ec4c36c4db4a9890f13466448faecacb389
buhara54 <buharavkf54@gmail.com>
2025-12-27 18:44:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zikir-takip-v2.8.3'; 
+const CACHE_NAME = 'zikir-takip-v2.8.4'; 
 const ASSETS = [
   './',
   './index.html',
@@ -70,6 +70,7 @@ self.addEventListener('fetch', (e) => {
       })
   );
 });
+
 
 
 
