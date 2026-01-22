a4f3c1cd4fa3468b092702377e61384bb9b022fe
buhara54 <buharavkf54@gmail.com>
2026-01-22 04:42:07 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const APP_VERSION = "v3.4";
+const CACHE_VERSION = "3.4";
+
+// HTML tarfında kullanmak için:
+// document.getElementById('uygulama-versiyon').innerText = APP_VERSION;
+
+// Service Worker tarafında kullanmak için:
+// self.importScripts('config.js');
+// const CACHE_NAME = "zikir-takip-" + CACHE_VERSION;
