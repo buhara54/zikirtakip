75646ec4bc9661d45679a21109c44b254f243b37
buhara54 <buharavkf54@gmail.com>
2026-02-04 21:22:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "v3.5.3";
-const CACHE_VERSION = "3.5.3";
+const APP_VERSION = "v3.5.7";
+const CACHE_VERSION = "3.5.7";
 
 // HTML tarfında kullanmak için:
 // document.getElementById('uygulama-versiyon').innerText = APP_VERSION;
@@ -7,3 +7,6 @@ const CACHE_VERSION = "3.5.3";
 // Service Worker tarafında kullanmak için:
 // self.importScripts('config.js');
 // const CACHE_NAME = "zikir-takip-" + CACHE_VERSION;
+
+const API_URL = "https://script.google.com/macros/s/AKfycbyN6UOTS9mM9KXWRfGxf3Snlg3U9s2s-SoVIZ7hX46on3f_vTJnw-GUTalad1LAny2i/exec";
+const API_URL_HK = "https://script.google.com/macros/s/AKfycby5cZOLZDCcDOyTANA9v8S5XZ03HzS0ydAu5eRR4BdOZ9qwPT0n_993J571h-3U-W0daQ/exec";
