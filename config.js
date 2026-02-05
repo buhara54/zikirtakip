41b2a964b96625d63710847dc3dfe1faeb6bc7c6
buhara54 <buharavkf54@gmail.com>
2026-02-05 17:50:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,4 @@ const CACHE_VERSION = "3.5.11";
 // const CACHE_NAME = "zikir-takip-" + CACHE_VERSION;
 
 const API_URL = "https://script.google.com/macros/s/AKfycbyN6UOTS9mM9KXWRfGxf3Snlg3U9s2s-SoVIZ7hX46on3f_vTJnw-GUTalad1LAny2i/exec";
-const API_URL_HK = "https://script.google.com/macros/s/AKfycby5cZOLZDCcDOyTANA9v8S5XZ03HzS0ydAu5eRR4BdOZ9qwPT0n_993J571h-3U-W0daQ/exec";
+const API_URL_HK = "https://script.google.com/macros/s/AKfycbyI3sD_F57FqUrM_cai6vJKzTic08D74JgyYNbitkK4VnbJyw_sdat-QO7O5FtxQjyM/exec";
