e531f305ec654d0c7b42651c2c21f91efcf7a0c8
buhara54 <buharavkf54@gmail.com>
2026-02-06 19:33:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "v3.5.16";
-const CACHE_VERSION = "3.5.16";
+const APP_VERSION = "v3.5.17";
+const CACHE_VERSION = "3.5.17";
 
 // HTML tarfında kullanmak için:
 // document.getElementById('uygulama-versiyon').innerText = APP_VERSION;
